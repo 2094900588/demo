036ffe7250ba588829749c56af5b1fee5e2ab490
abc1 <2094900588.com>
2023-05-10 10:50:59 +0800

    redis

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+        redisTemplate.opsForValue().set("test","6666");
+        String test = redisTemplate.opsForValue().get("test");
+        System.out.println(test);
     }
 
 }
